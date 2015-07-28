1df9c90fdc06c3d1bc3cdaf0a6d08a12e96c1260
Leonardo Bernardes <leonardo.bernardes@sap.com>
2015-07-28 19:24:28 -0300

    Enablement for debbuging odata. For using it, add ?odata-debug=json to the odata url.

--- logistics/logistics-service/src/main/java/sap/logistics/service/ODataLogistics.java
+++ logistics/logistics-service/src/main/java/sap/logistics/service/ODataLogistics.java
@@ -3,6 +3,8 @@ package sap.logistics.service;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import org.apache.olingo.odata2.api.ODataCallback;
+import org.apache.olingo.odata2.api.ODataDebugCallback;
 import org.apache.olingo.odata2.jpa.processor.api.ODataJPAContext;
 import org.apache.olingo.odata2.jpa.processor.api.ODataJPAServiceFactory;
 import org.apache.olingo.odata2.jpa.processor.api.exception.ODataJPARuntimeException;
@@ -19,5 +21,15 @@ public class ODataLogistics extends ODataJPAServiceFactory {
 		oDataJPAContext.setPersistenceUnitName(PUNIT_NAME);
 		return oDataJPAContext;
 
+	}
+	
+	@SuppressWarnings("unchecked")
+	public <T extends ODataCallback> T getCallback(final Class<? extends ODataCallback> callbackInterface)
+	{ 
+	  return (T) (callbackInterface.isAssignableFrom(ScenarioErrorCallback.class) ? 
+	    new ScenarioErrorCallback() : 
+	      callbackInterface.isAssignableFrom(ODataDebugCallback.class) ? 
+	        new ScenarioDebugCallback() : 
+	        super.getCallback(callbackInterface));
 	}
 }
